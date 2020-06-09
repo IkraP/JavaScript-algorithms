2f2d2dc92bd8c54b90d75f267243830c591215a4
ikraP <ikrapervaiz@gmail.com>
2020-06-09 11:27:44 +0100

    testing using Jest

--- data_structures/queue/queue.test.js
+++ data_structures/queue/queue.test.js
@@ -1,7 +1,26 @@
 const Queue = require("./queue");
 
-test("should have a Queue class with a maxSize property", () => {
-  const maxSize = 1;
-  const testQueue = new Queue(maxSize);
-  expect(testQueue.maxSize).toBe(1);
+describe("Queue data structure", () => {
+  test("should return a maxSize property that is passed as an argument when new Queue is created", () => {
+    const maxSize = 1;
+    const testQueue = new Queue(maxSize);
+    expect(testQueue.maxSize).toBe(1);
+  });
+  test("should return a storage property when a new Queue is creates - returns an empty object", () => {
+    const maxSize = 5;
+    const testQueue = new Queue(maxSize);
+    expect(testQueue.storage).toEqual({});
+  });
+  test("should have a front and back property that represents the index of the front and back of the queue respectively", () => {
+    const maxSize = 10;
+    const testQueue = new Queue(maxSize);
+    expect(testQueue).toHaveProperty("frontIndex");
+    expect(testQueue).toHaveProperty("backIndex");
+  });
+  test("should have an enQueue method that adds one item to the back of the queue providing the maxSize has not be fulfilled", () => {
+    const maxSize = 10;
+    const testQueue = new Queue(maxSize);
+    testQueue.enQueue("Ikra");
+    expect(testQueue.storage).toEqual({ "0": "Ikra" });
+  });
 });
