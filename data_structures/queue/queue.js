6977203762a00a1ca0c2999dfd2d4f775c24d98a
ikraP <ikrapervaiz@gmail.com>
2020-06-09 17:45:18 +0100

    additional methods to queue data structure and testing

--- data_structures/queue/queue.js
+++ data_structures/queue/queue.js
@@ -5,6 +5,44 @@ class Queue {
     this.frontIndex = 0;
     this.backIndex = -1;
   }
+  storageLength() {
+    /* function calculates the length of the queue by subtracting the last index and front index + 1
+    Example: backIndex (+3 items) = -1 + 3 = 2 therefore 3 items in the queue (0, 1, 2)
+    frontIndex = 0
+    therefore length: 2 - 0 + 1 = 3
+     */
+    return this.backIndex - this.frontIndex + 1;
+  }
+  enQueue(item) {
+    /* function to add an item to the back of the queue, only if queue has not surpassed its maxSize.
+  backIndex is increased by one to reflect the addition of item to queue and then item added to storage at the position of the backIndex */
+    if (this.storageLength() < this.maxSize) {
+      this.storage[++this.backIndex] = item;
+    }
+  }
+
+  deQueue() {
+    /* function to remove items from the front of the queue, only if the queue has items in storage.
+    The items is deleted from the frontIndex position adn the frontIndex is increased by 1 as items are moved down the queue */
+    if (this.storageLength() !== 0) {
+      delete this.storage[this.frontIndex++];
+    }
+  }
+
+  isEmpty() {
+    // function to return a boolean of true when queue is empty
+    return !this.storageLength();
+  }
+
+  isFull() {
+    // function to return a boolean of true when queue has reached maxSize
+    return this.storageLength() === this.maxSize ? true : false;
+  }
+
+  peek() {
+    //function to return the item at the front of the queue without removal
+    return this.storage[this.frontIndex];
+  }
 }
 
 module.exports = Queue;
--- data_structures/queue/queue.test.js
+++ data_structures/queue/queue.test.js
@@ -23,4 +23,74 @@ describe("Queue data structure", () => {
     testQueue.enQueue("Ikra");
     expect(testQueue.storage).toEqual({ "0": "Ikra" });
   });
+  test("should have a storageLength method that returns the length of the storage in the queue", () => {
+    const maxSize = 2;
+    const testQueue = new Queue(maxSize);
+    testQueue.enQueue("hello");
+    testQueue.enQueue("world");
+    expect(testQueue.storage).toEqual({ 0: "hello", 1: "world" });
+    expect(testQueue.storageLength()).toBe(2);
+  });
+  test("should not add items to the queue when the maxSize has been exceeded", () => {
+    const maxSize = 2;
+    const testQueue = new Queue(maxSize);
+    testQueue.enQueue("hello");
+    testQueue.enQueue("world");
+    testQueue.enQueue("computer");
+    expect(testQueue.storage).toEqual({ 0: "hello", 1: "world" });
+    expect(testQueue.storageLength()).toBe(2);
+  });
+  test("should increase the backIndex in respect to amount of items that have been added to the queue", () => {
+    const maxSize = 3;
+    const testQueue = new Queue(maxSize);
+    testQueue.enQueue("hello");
+    testQueue.enQueue("world");
+    expect(testQueue.backIndex).toBe(1);
+    expect(testQueue.storageLength()).toBe(2);
+  });
+  test("should have a deQueue function that removes items from the front of the queue, only if items are present in the queue", () => {
+    const maxSize = 3;
+    const testQueue = new Queue(maxSize);
+    testQueue.enQueue("hello");
+    testQueue.enQueue("world");
+    testQueue.deQueue();
+    expect(testQueue.storageLength()).toBe(1);
+    expect(testQueue.storage).toEqual({ 1: "world" });
+  });
+  it("should update the front and back index when removing items from the front of the queue", () => {
+    const maxSize = 3;
+    const testQueue = new Queue(maxSize);
+    testQueue.enQueue("Ikra");
+    testQueue.enQueue("hello");
+    testQueue.enQueue("world");
+    testQueue.deQueue();
+    expect(testQueue.frontIndex).toBe(1);
+    expect(testQueue.backIndex).toBe(2);
+    expect(testQueue.storageLength()).toBe(2);
+  });
+  test("should have an isEmpty method that will return a boolean of true or false reflecting queue storage", () => {
+    const maxSize = 1;
+    const testQueue = new Queue(maxSize);
+    expect(testQueue.isEmpty()).toBe(true);
+    testQueue.enQueue("Ikra");
+    expect(testQueue.isEmpty()).toBe(false);
+    expect(testQueue.storageLength()).toBe(1);
+  });
+  test("should have an isFull method that will return a boolean of true when the queue is full and false when the queue has not reached its maxSize", () => {
+    const maxSize = 2;
+    const testQueue = new Queue(maxSize);
+    expect(testQueue.isFull()).toBe(false);
+    testQueue.enQueue("Ikra");
+    testQueue.enQueue("hello");
+    expect(testQueue.storageLength()).toBe(2);
+    expect(testQueue.isFull()).toBe(true);
+  });
+  test("should have a peek method that returns the item that is at the front of the queue without removal from queue", () => {
+    const maxSize = 2;
+    const testQueue = new Queue(maxSize);
+    testQueue.enQueue("Ikra");
+    testQueue.enQueue("hello");
+    expect(testQueue.peek()).toBe("Ikra");
+    expect(testQueue.storageLength()).toBe(2);
+  });
 });
